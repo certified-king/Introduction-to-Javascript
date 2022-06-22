6d329b02795c55e07dc3e4abc2bf91d0419acca0
Michael Onyike <81037799+certified-king@users.noreply.github.com>
2022-06-22 09:28:19 +0100

    This is a Zuri Assigment for Intro to JS

---
+++ index.js
@@ -0,0 +1,16 @@
+// This is JS program to display fullname, height and country. Zuri to the world.
+
+// Prompts user to enter full name - I wanted it to be more interactive, but decide to stick to instructions.
+// let fullName = prompt('What is your full name?');
+let fullName = "Michael Onyike";
+
+// Prompts user to enter height - I wanted it to be more interactive, but decide to stick to instructions.
+// let height = prompt('Let us know your height');
+let height = "1.76 Meters";
+
+// Prompts user to enter their country - I wanted it to be more interactive, but decide to stick to instructions.
+// let country = prompt('Which country are you from?');
+let country = "Nigeria";
+
+// This will print their info provided above.
+document.write('Welcome' + " " + fullName + "<br/>" + 'Your height is:' + " " + height + "<br/>" + 'And you are from:' + " " + country);
